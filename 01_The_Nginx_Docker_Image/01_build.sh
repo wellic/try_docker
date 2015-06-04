3bb4ce00e30d66c3ceaa440b07cfe647b7b20f38
Valerii Savchenko <valerii.s@giantleaplab.com>
2015-06-04 18:05:37 +0300

    Create all tasks of studies article

---
+++ 01_The_Nginx_Docker_Image/01_build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd images/nginx 
+
+docker build -t tutorial/nginx .
---
+++ 08_The_MySQL_Docker_Container/02_exec.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#docker exec -it [CONTAINER_ID] /bin/bash
+
+docker exec -it "$1" /bin/bash
